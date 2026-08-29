3ae1456229ac8336504db52eb3129e6df0b70bd7
= <=>
2026-08-29 12:26:47 -0300

    adicionando o projeto financeiro-api

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function () {
+    return  response()->json([
+        'message' => 'acesso com sucesso',
+        'data' => [
+            'usuario' => 'Cleiton'
+        ]
+    ], 200);
+});
