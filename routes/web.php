8933cdab5df5da3dae603c10a52e7c19cd0f02eb
Cleiton <cleiton_601@hotmail.com>
2026-08-31 16:50:25 -0300

    feat(Autenticacao) criando a autenticacao do Laravel SPA

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return abort(303, 'Ola');
 });
+
+Route::post('/login', [LoginController::class, 'login']);
